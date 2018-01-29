051ae6313f020c1ecf22ac6ef4562933bdba6abf
Taylor Liang <bu213200@gmail.com>
2018-01-29 15:30:23 +0800

    Update

--- FindLargeFile/Program.cs
+++ FindLargeFile/Program.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -10,6 +11,24 @@ namespace FindLargeFile
     {
         static void Main(string[] args)
         {
+            string path;
+
+            do
+            {
+                Console.WriteLine("Path:");
+                path = Console.ReadLine();
+            } while (!Directory.Exists(path));
+
+            foreach (var item in Directory.GetFiles(path, "*", SearchOption.AllDirectories))
+            {
+                if (new FileInfo(item).Length > 100 * 1024 * 1024)
+                {
+                    Console.WriteLine(item + " is over size 100 MB.");
+                }
+            }
+            Console.WriteLine("End.");
+
+            Console.ReadLine();
         }
     }
 }
